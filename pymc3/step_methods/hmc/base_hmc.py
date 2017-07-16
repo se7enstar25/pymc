se7enstar25/pymc

commit 6f58dbfdeeb21d682fecf435919906f0d3289ce2

Return identity matrix if no scaling provided (#2236)

pymc3/step_methods/hmc/base_hmc.py
@@ -4,7 +4,8 @@ from .trajectory import get_theano_hamiltonian_functions
 from pymc3.tuning import guess_scaling
 from pymc3.model import modelcontext, Point
 from .quadpotential import quad_potential
-from pymc3.theanof import inputvars, make_shared_replacements
+from pymc3.theanof import inputvars, make_shared_replacements, floatX
+import numpy as np
 
 
 class BaseHMC(ArrayStepShared):
@@ -41,7 +42,7 @@ class BaseHMC(ArrayStepShared):
         vars = inputvars(vars)
 
         if scaling is None and potential is None:
-            scaling = model.test_point
+            scaling = floatX(np.ones(model.dict_to_array(model.test_point).size))
 
         if isinstance(scaling, dict):
             scaling = guess_scaling(Point(scaling, model=model), model=model, vars=vars)

commit 362d736ed6377ea09fd9cc67b24a09da77c443be

Fixed error introduced by 6f58dbf (#2419)
* Fixed error introduced by 6f58dbf in which the scaling variable is initialized as an array instead of a dict. This commit maintains the intentions of the 6f58dbf commit by initializing 'scaling' to a dict of arrays of identity with type floatX.

* Array of ones with shape of selected vars

This commit is actually preserves the intention of the previous commit as it avoids the guess_scaling function. It still maintains that the shape of scaling must be the size of the specified variables instead of the whole model.

* Removed unnecessary function imports to namespace that were previously used for dict to array bijection

pymc3/step_methods/hmc/base_hmc.py
@@ -42,7 +42,9 @@ class BaseHMC(ArrayStepShared):
         vars = inputvars(vars)
 
         if scaling is None and potential is None:
-            scaling = floatX(np.ones(model.dict_to_array(model.test_point).size))
+            varnames = [var.name for var in vars]
+            size = sum(v.size for k, v in model.test_point.items() if k in varnames)
+            scaling = floatX(np.ones(size))
 
         if isinstance(scaling, dict):
             scaling = guess_scaling(Point(scaling, model=model), model=model, vars=vars)